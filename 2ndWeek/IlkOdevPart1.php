a13d076b2cbcf86dd56b98c5109960e2e7996d7b
Engindeniz00 <83114158+Engindeniz00@users.noreply.github.com>
2021-08-31 00:04:51 +0300

    Hafta2Tekrar
    İç içe array kullanarak istenilen değeri çekme

--- 2ndWeek/IlkOdevPart1.php
+++ 2ndWeek/IlkOdevPart1.php
@@ -1,6 +1,6 @@
 <?php
 
-$arr = [
+$arr = [ // array değerleri
     'name' => 'gokhan',
     'surname' => 'yener',
     'sports' => [
@@ -10,27 +10,28 @@ $arr = [
             'judo' => 'yes',
             'boxing' => 'no'
         ]
-      ]
-    ];
+    ]
+];
 
-$kont = true;
+
+$kont = true; // key'in array içersinde olup olmadığını kontrol ediyor
 
 function GetThat($arr,$key){
     global $kont;
-    if(is_array($arr)){
-        foreach($arr as $key_gen => $value){
-            if(is_array($value)){
-                if($key_gen === $key){
+    if(is_array($arr)){ // array olup olmadığı kontrolü
+        foreach($arr as $key_gen => $value){ // key ve value değerlerinin array içersinde almamızı sağlar
+            if(is_array($value)){ // değerin array olup olmadığı kontrolü
+                if($key_gen === $key){ // array ise key olup olmadığı
                     $kont = false;
-                    print_r($value);
+                    print_r($value); // değeri yazdır
                 }
                 else
                 {
-                    GetThat($value,$key);
+                    GetThat($value,$key); // array ise ama key değil ise GetThat fonksiyonu geri çağırır
                 }
             }
             else{
-                if($key_gen === $key){
+                if($key_gen === $key){ // değer array değilse ve key doğru ise değeri echo ile yazdır
                     $kont = false;
                     echo $value;
                 }
